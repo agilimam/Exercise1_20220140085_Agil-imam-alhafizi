1521d3c48dc22f16c08d3ca41f263b81a742cd17
agilimam <agilimamalhafizi04@gmail.com>
2024-03-31 13:42:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function showAlert(){
+    alert(Date(), tampil(), show())
+    Date()
+
+    let tanggal = new Date();
+    document.getElementById("hasil").innerHTML = tanggal;
+
+    function tampil(){
+        alert("Berikut adalah beberapa tempat wisata yang ada di kerinci jambi.")
+    }
+
+    function show(){
+        alert("Bukann hanya itu masih terdapat banyak tempat wisata yang berada di kerinci jambi")
+    }
+}
